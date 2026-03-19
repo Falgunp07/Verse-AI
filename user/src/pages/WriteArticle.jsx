2ed942ce5013b59cb3ecf93df6082a0a2c7819ba
Falgun <falgunpatel071@gmail.com>
2026-03-19 21:05:11 +0530

    container height error fixed

--- user/src/pages/WriteArticle.jsx
+++ user/src/pages/WriteArticle.jsx
@@ -72,7 +72,7 @@ const WriteArticle = () => {
 
       </form>
       {/* right column  */}
-      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 h-[600px]'>
         <div className='flex items-center gap-3'>
           <Edit className='w-5 h-5 text-[#4A7AFF]' />
           <h1 className='text-xl font-semibold'>Generated article</h1>
@@ -87,7 +87,7 @@ const WriteArticle = () => {
           </div>
 
         ) : (
-          <div className='mt-3 h-full overflow-y-scroll text-sm text-slate-600'>
+          <div className='mt-3 flex-1 min-h-0 overflow-y-auto text-sm text-slate-600 pr-1'>
             <div className='reset-tw'>
               <Markdown>{content}</Markdown>
             </div>
